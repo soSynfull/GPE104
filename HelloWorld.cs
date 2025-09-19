53b3847854e546f39f5c107b8f2a3dbbd23b4750
soSynfull <marquis.a.smith00@gmail.com>
2025-09-18 17:40:20 -0700

    GPE104 Project 1 video and Script Commit

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
